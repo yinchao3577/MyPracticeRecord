ab6e7d45a0f9f55fb8a3e66e88ea212f233fe9f2
buhe <yinchao3577@qq.com>
2020-01-30 19:03:56 +0800

    SpringBoot集成Shiro实现认证和授权

---
+++ SpringBoot_Integrate_Shiro/src/test/java/com/cf/shiro1/Shiro1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cf.shiro1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shiro1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
